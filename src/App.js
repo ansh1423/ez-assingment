9f9c00c84bcee2979a71c2addd5c6a597c786e24
ansh1423 <anshyadav42495@gmail.com>
2025-03-23 14:45:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -121,7 +121,10 @@ function App() {
               <div className='plevel'>
               <div className="service-icon">
                
-<img src={firstImage} alt="Description" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <polygon points="23 7 16 12 23 17 23 7"></polygon>
+                  <rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect>
+                </svg>
               </div>
               <h3 className="service-title">Presentation Design</h3>
               </div>
